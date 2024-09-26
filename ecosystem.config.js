139029250a887415d990f5cd5f023ece4a2d55ce
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-09-26 02:09:28 +0200

    pm2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'kachabazar-store',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: "abh_customer",
+      script: "npm",
+      args: "run start",
+      instances: "max",
+      cwd: "/home/azureuser/abh_customer",
+      watch: true,
+      ignore_watch: ["node_modules"],
+      max_restarts: 5,
+      autorestart: true,
+      log_date_format: "YYY-MM-DD HH:mm:ss",
+      env: {
+        NODE_ENV: "production",
       },
-    ],
-  };
-  
+    },
+  ],
+};
